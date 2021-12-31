7a6f4c837aef03c057213614e01be0716aba39bc
FrogDog56 <83395400+FrogDog56@users.noreply.github.com>
2021-12-31 16:25:47 +0000

    Add files via upload

--- about.php
+++ about.php
@@ -1,20 +1,112 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
+    <link rel="stylesheet" href="stylesheets/bootstrap.css">
+    <link rel="stylesheet" href="stylesheets/style.css?v=<?php echo time(); ?>">
 </head>
-<body>
-    <header>
-        <nav><?php require 'html/nav.html'; ?></nav>
-    </header>
-    <main>
-        <h1>About</h1>
+<style>
+@media(min-width: 768px) {
+    main {
+        margin-left: 15%;
+        margin-right: 15%;
+    }
+
+    #welcome {
+        padding-left: 3%;
+    }
+
+    #text {
+        padding-left: 4.25%;
+        padding-right: 4.25%;
+    }
+
+    .main-text {
+        padding-right: 3%;
+    }
+
+    .lower-nav {
+        padding-left: 4.5%;
+        padding-top: 3%;
+        padding-bottom: 1.5%;
+        font-size: 1.125rem;
+    }
+
+}
+
+#a {
+    padding-bottom: 1%;
+}
+
+.c-form {
+    padding-left: 3%;
+    padding-right: 3%;
+}
+
+@media(max-width: 768px) {
+    .lower-nav {
+        width: 0px;
+        height: 0px;
+    }
+
+    #a {
+        font-size: 0px;
+    }
+
+    #b {
+        font-size: 0px;
+    }
+
+    .title {
+        text-align: center;
+        margin: 0;
+        padding: 0;
+    }
+
+    #text {
+        text-align: center;
+        padding-left: 3%;
+        padding-right: 3%;
+    }
+}
+</style>
+
+<body class="bg-grey">
+    <?php require 'html/loginbar.html'; ?>
+    <br>
+    <main class="bg-grey">
+        <div class="flex-child">
+            <h1 class="title"><strong>Logmas</strong> Ltd.</h1>
+        </div>
+        <br>
+        <section class="bg-white">
+            <nav class="bg-grey-light">
+                <?php require 'html/nav.html'; ?>
+                <div class="lower-nav">
+                    <h2 id="a">About Us</h2>
+                    <p id="b"><a id="b" href="index.php" class="highlight">Home</a> / About</p>
+                </div>
+            </nav>
+            <br>
+            <div id="text">
+                <p>We are a TY mini company based in Dublin. All of our products are handmade from Irish raw materials.
+                    All of our products are reasonably priced.
+                </p>
+                <br>
+                <p>We will be selling and delivering our products until the 24th of December.</p>
+            </div>
+            <br>
+        </section>
+        <footer class="bg-dark">
+            <?php require "html/foot.html"?>
+        </footer>
     </main>
-    <footer>
-        <?php require 'html/foot.html'; ?>
-    </footer>
 </body>
+
+
+
 </html>
--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
     // Message variables
     $msg = '';
     $msgClass = '';
-
+    
     // Check For Submit
     if(filter_has_var(INPUT_POST, "submit")){
 
@@ -20,7 +20,7 @@
             } else {
                 //passed
                 //recipient Email
-                $toEmail = 'Christmasdecorminicompany@gmail.com';
+                $toEmail = 'whelankww05@gmail.com';
                 $subject = "Contact Request From $name";
                 $body = '<h2>Contact Request<h2>
                 <h4>Name</h4><p>'.$name.'</p>
@@ -54,39 +54,140 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us</title>
     <link rel="stylesheet" href="stylesheets/bootstrap.css">
+    <link rel="stylesheet" href="stylesheets/style.css?v=<?php echo time(); ?>">
+
 </head>
-<body>
-    <nav>
-    <?php require 'html/nav.html'?>
-    </nav>
+<style>
+@media(min-width: 768px) {
+    main {
+        margin-left: 15%;
+        margin-right: 15%;
+    }
+
+    #welcome {
+        padding-left: 3%;
+    }
+
+    #text {
+        padding-left: 4.25%;
+        padding-right: 4.25%;
+    }
+
+    .main-text {
+        padding-right: 3%;
+    }
+
+    .lower-nav {
+        padding-left: 4.5%;
+        padding-top: 3%;
+        padding-bottom: 1.5%;
+        font-size: 1.125rem;
+    }
+
+}
+
+#a {
+    padding-bottom: 1%;
+}
+
+.c-form {
+    padding-left: 3%;
+    padding-right: 3%;
+}
+
+@media(max-width: 768px) {
+    .lower-nav {
+        width: 0px;
+        height: 0px;
+    }
+
+    #a {
+        font-size: 0px;
+    }
+
+    #b {
+        font-size: 0px;
+    }
+
+    .title {
+        text-align: center;
+        margin: 0;
+        padding: 0;
+    }
+
+    #text {
+        text-align: center;
+        padding-left: 3%;
+        padding-right: 3%;
+    }
+
+    .container {
+        text-align: center;
+    }
+
+}
+</style>
+
+<body class="bg-grey">
+    <?php require 'html/loginbar.html'; ?>
     <br>
-    <div class="container">
-        <?php if($msg != ''): ?>
-            <div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?></div>
-        <?php endif; ?>
-        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-            <div class="form-group">
-                <label>Name</label>
-                <input type="text" name="name" class="form-control" value="<?php echo isset($_POST['name']) ? $name : ''; ?>">
-            </div>
-            <div class="form-group">
-                <label>Email</label>
-                <input type="text" name="email" class="form-control" value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
-            </div>
-            <div class="form-group">
-                <label>Message</label>
-                <textarea name="message" class="form-control"><?php echo isset($_POST['message']) ? $message : ''; ?></textarea>
+    <main class="bg-grey">
+        <div class="flex-child">
+            <h1 class="title"><strong>Logmas</strong> Ltd.</h1>
+        </div>
+        <br>
+        <section class="bg-white">
+            <nav class="bg-grey-light">
+                <?php require 'html/nav.html'; ?>
+                <div class="lower-nav">
+                    <h2 id="a">Contact Us</h2>
+                    <p id="b"><a id="b" href="index.php" class="highlight">Home</a> / Contact</p>
+                </div>
+            </nav>
+
+            <br>
+            <div id="text">
+                <p>Want to give us some feedback? Use the form below to get in touch with us about your experience and
+                    we’ll do our best to help out.</p>
             </div>
             <br>
-            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
-    
+            <div class="container">
+                <?php if($msg != ''): ?>
+                <div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?></div>
+                <?php endif; ?>
+                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?> " class="c-form">
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" name="name" class="form-control"
+                            value="<?php echo isset($_POST['name']) ? $name : ''; ?>">
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="text" name="email" class="form-control"
+                            value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
+                    </div>
+                    <div class="form-group">
+                        <label>Message</label>
+                        <textarea name="message"
+                            class="form-control"><?php echo isset($_POST['message']) ? $message : ''; ?></textarea>
+                    </div>
+                    <br>
+                    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+            <br>
+        </section>
+        <footer class="bg-dark">
+            <?php require "html/foot.html"?>
+        </footer>
+    </main>
 </body>
+
 </html>
